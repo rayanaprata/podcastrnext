ce4ae948c97f92cdd02b06610452f78675a98b63
Rayana Prata <50561896+rayanaprata@users.noreply.github.com>
2021-04-21 18:46:17 -0300

    aula 2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,61 @@
-import React from "react";
-import Header from "../components/Header";
+// SPA
+// SSR
+// SSG
 
-export default function Home() {
+export default function Home(props) {
+
   return (
-    <Header />
+    <div>
+      <h1>Index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </div>
   )
+
 }
+
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  }
+
+}
+
+/* REQUISIÇÃO MODELO SPA 
+
+// import para SPA
+// import { useEffect } from "react" 
+
+  // (efeitos colaterais) -> dispara algo, sempre que algo mudar na aplicação
+  useEffect(() => {
+    fetch('http://localhost:3333/episodes')
+    .then(response => response.json())
+    .then(data => console.log(data))
+  }, [])
+
+  /* primeiro parametro: o que eu quero executar // segundo parametro: qundo quero executar */
+
+  // REQUISIÇÃO MODELO SSR
+
+/* REQUISIÇÃO MODELO SSR
+
+Passar props como parametro na fucntion Home
+
+export async function getServerSideProps() {
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+
+  return {
+    props: {
+      episodes: data,
+    }
+  }
+
+}
+
+*/
